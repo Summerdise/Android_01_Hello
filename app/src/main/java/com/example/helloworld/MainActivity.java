9d4f44c915fef7400edcbad9ec5afe8b53fbf689
Summerdise <350464517@qq.com>
2020-08-11 23:57:01 +0800

    [zmc] feat: add jump function from main to frame activity

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button frameEntryBtn = findViewById(R.id.mainBtn2);
+        frameEntryBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FrameActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
