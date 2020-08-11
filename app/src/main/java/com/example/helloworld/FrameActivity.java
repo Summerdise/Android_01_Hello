cd7fc737d99184e4769e7315a9401792e79ab01b
Summerdise <350464517@qq.com>
2020-08-12 00:15:13 +0800

    [zmc] feat: add two functions for backing to main activity

--- app/src/main/java/com/example/helloworld/FrameActivity.java
+++ app/src/main/java/com/example/helloworld/FrameActivity.java
@@ -2,7 +2,10 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class FrameActivity extends AppCompatActivity {
 
@@ -10,6 +13,25 @@ public class FrameActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frame_layout);
+
+        Button frameBackBtn = findViewById(R.id.frameBtn1);
+        frameBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FrameActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        View frameTitle = findViewById(R.id.frameTitle);
+        frameTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(FrameActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
 }
